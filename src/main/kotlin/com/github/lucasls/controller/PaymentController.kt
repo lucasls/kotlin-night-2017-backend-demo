4acdc68c772343ff226f7eb7391bfb2157e8ead7
snaplucas <lucas.chapola@gmail.com>
2017-08-31 22:49:14 -0300

    optmizing imports

--- src/main/kotlin/com/github/lucasls/controller/PaymentController.kt
+++ src/main/kotlin/com/github/lucasls/controller/PaymentController.kt
@@ -4,7 +4,6 @@ import com.github.lucasls.model.Payment
 import com.github.lucasls.service.PaymentRequest
 import com.github.lucasls.service.PaymentService
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
